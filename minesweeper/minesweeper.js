3f120eea013ad574d7faac2c08516311a219bd5d
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-05-12 15:12:18 +0400

    feat: add count

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -16,7 +16,7 @@ for (let i = 1; i <= quantity; i++) {
 
 const chilCell = Array.from(mineArea.children);
 
-console.log(chilCell);
+//console.log(chilCell);
 function getRandomNumbers(quantity, range) {
   const numbers = [];
   for (let i = 1; i <= quantity; i++) {
@@ -27,9 +27,53 @@ function getRandomNumbers(quantity, range) {
 const bomb = getRandomNumbers(10, quantity);
 console.log(bomb);
 
+
+const buttons = document.getElementsByClassName('btn');
+
+//=========Координаты======
 mineArea.addEventListener('click', (event) => {
   const btnClass = event.target;
-  if (btnClass.classList.contains('btn')) {
-    // event.target.innerHTML
+  const indx = chilCell.indexOf(event.target);
+  for (let i = 0; i < buttons.length; i++) {
+    const x = Math.floor(i / 10) + 1; 
+    const y = (i % 10) + 1; 
+    buttons[i].dataset.coordinates = `${x}:${y}`;
+    buttons[i].addEventListener('click', (event) => {
+      const button = event.target;
+      const coordinate = button.dataset.coordinates;
+    });
   }
+  
+  if (btnClass.classList.contains('btn')) {
+    const coordinate = btnClass.dataset.coordinates;
+    const [x, y] = coordinate.split(":");
+    const coorBtn = Number((y-1)%10) + Number((x-1)*10);
+    console.log(coorBtn,1000);
+      if(bomb.includes(coorBtn)){
+       event.target.innerHTML = '9';
+       event.target.disabled = true;
+      }
+      else{
+        event.target.innerHTML =  count(x, y);
+        event.target.disabled = true;
+       }
+    //console.log(`Координаты кнопки: ${coordinate}`);
+    //console.log(indx);
+
+    }
 });
+
+function count(x, y) {
+  let count = 0;
+  for(let i = -1; i<= 1; i++) {
+    for(let j = -1; j<= 1; j++) {
+      let coorBtn = Number((y-1+i)%10) + Number((x-1+j)*10);   
+      
+      if(bomb.includes(coorBtn)) {
+        count++;
+        console.log(coorBtn,'bomb');
+      }
+    }
+  }
+  return count;
+}
