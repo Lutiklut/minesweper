4f7fc97eb75329363d328824bb9ba7be86079c6d
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-05-18 23:33:43 +0400

    feat: not worked storage

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -12,18 +12,20 @@ modal.appendChild(info);
 const close = document.createElement('DIV');
 close.classList.add('close');
 modal.appendChild(close);
-close.innerHTML = '&#9421;';
-
-
-
+close.innerHTML = 'Закрыть';
 
 close.addEventListener('click',()=> { 
   popup.style.display = 'none';
   document.body.style.overflow = "";
   smile.disabled = false;
+  popup.style.zIndex ='-100';
 });
 
-
+const audi = document.createElement('audio');
+audi.src = "audio/start.mp3";
+document.body.appendChild(audi);
+let src = 'audio/start.mp3'
+soundClick(src)
 
 const headerName = document.createElement('H1');
 headerName.classList.add('name');
@@ -42,17 +44,9 @@ flag.innerHTML = 10;
 const smile = document.createElement('BUTTON');
 smile.classList.add('smile');
 totalArea.appendChild(smile);
+smile.innerHTML = '	&#129488;';
 
 
-const image = document.createElement('img');
-image.classList.add('img');
-image.src  = 'img/game.jpeg';
-image.alt = 'Think smile';
-smile.appendChild(image);
-
-smile.addEventListener('click',()=> {
-    document.location.reload();
-});
 
 const stopwatch = document.createElement('DIV');
 stopwatch.classList.add('timer');
@@ -92,8 +86,8 @@ var timerInterval;
 var ms = 0;
 var sec = document.getElementById('second');
 var min = document.getElementById('minute');
-
-function startGame() {
+let startGame = false;
+function startGameTimer() {
     stopGame();
     timerInterval = setInterval(function() {
       timer += 1/60;
@@ -119,6 +113,7 @@ function startGame() {
         minute.innerHTML = minuteF;
       }
     }, 1000/60);
+    startGame = true;
   }
   
   function stopGame() {
@@ -126,99 +121,174 @@ function startGame() {
 
   }
 
-
-function getRandomNumbers(quantity, range) {
+let bombIsReady = false;
+function getRandomNumbers(bom, range, z) {
   const numbers = [];
-  while (numbers.length < quantity) {
+  while (numbers.length < bom) {
     const randomNumber = Math.floor(Math.random() * range);
-    if (!numbers.includes(randomNumber)) {
+    if (!numbers.includes(randomNumber) && !numbers.includes(z)) {
       numbers.push(randomNumber);
     }
   }
-    return numbers;
+  bombIsReady = true;  
+  return numbers;
 }  
-const bomb = getRandomNumbers(10, quantity);
-console.log(bomb);
+
 
 const buttons = document.getElementsByClassName('btn');
 const mineAreaDom=document.querySelector('.area')
 
 //=========Координаты======
 for (let i = 0; i < buttons.length; i++) {
-    const x = Math.floor(i / 10) + 1; 
-    const y = (i % 10) + 1; 
-    buttons[i].dataset.coordinates = `${x}:${y}`;
+  const x = Math.floor(i / 10) + 1; 
+  const y = (i % 10) + 1; 
+  buttons[i].dataset.coordinates = `${x}:${y}`;
 }
 let countOpenCells = [];
+let bomb =[]
+let countClick = 0;
 
-mineArea.addEventListener('click', (event) => {
-    startGame()
+let disabledArrey = [];
+let stateCells = [];
+
+let historyGame = false;
+
+function localStorageFunc () {
+    //console.log(localStorage.getItem('bombArrey').split('.') != 0, "localStorage.getItem('bombArrey').split('.') != 0")
+  if (localStorage.getItem('bombArrey') !== '') {
+    bombIsReady = true;
+    bomb = localStorage.getItem('bombArrey').split(',');
+ // console.log(localStorage.getItem('bombArrey'), 'bombArrey');
+    let inner =localStorage.getItem('inner').split(',');
+    console.log(inner, 'inner');
+    let dis = localStorage.getItem('disabled').split(',');
+
+    console.log(dis, "dis");
+    dis = dis.map(el => Number(el));
+    console.log(dis, "dis");
+    for (let i = 0;  i < inner.length; i++) {
+     
+        console.log(typeof(dis[i]), "typeof(dis[i])[i]");  
+      if(dis.includes(i)) {
+        console.log(inner[i], "inner[i]"); 
+        console.log(localStorage.getItem('inner').split('.')[i], "localStorage.getItem('inner').split('.')[i]");  
+        mineAreaDom.children[i].innerHTML = inner[i];
+      
+        mineAreaDom.children[i].disabled = true;}
+    
+  }}
+  historyGame = true;
+}
+
+if (!historyGame) {
+    localStorageFunc();
+    console.log(111) 
+}
+mineAreaDom.addEventListener('click', (event) => {
+  event.stopPropagation();
+  src = 'audio/click.mp3';
+  soundClick(src);
+  if (event.target) {
+    countClick++;
+  }
+  if (!startGame) {
+    startGameTimer()
+  }
+
   const btnClass = event.target;
-  
-  if (btnClass.classList.contains('btn')) {
-    const coordinate = btnClass.dataset.coordinates;
-    let [x, y] = coordinate.split(":");
-    const coorBtn = Number((y-1)%10) + Number((x-1)*10);
-    console.log('coordinate: ' + coorBtn);
-
-      if(bomb.includes(coorBtn)){
-        event.target.innerHTML = '&#164;';
-        event.target.disabled = true;
-        for(let mine of bomb) {
-          mineAreaDom.children[mine].innerHTML = '&#128163;';
-        };
-        for(let i = 0; i < mineAreaDom.children.length; i++) {
+  const coordinate = btnClass.dataset.coordinates;
+  let [x, y] = coordinate.split(":");
+  const coorBtn = Number((y-1)%10) + Number((x-1)*10);
+  if (!bombIsReady){
+    bomb = getRandomNumbers(10, quantity, coorBtn);
+    console.log(bomb);
+  }
+  localStorage.setItem('bombArrey', bomb.toString()) ;
+  localStorage.setItem('bombArrey', bomb.toString()) ;
+  console.log(localStorage.getItem('bombArrey'), 'bombArrey') 
+  if(bomb.includes(coorBtn)){
+    event.target.innerHTML = '&#164;';
+    event.target.disabled = true;
+    for(let mine of bomb) {
+      mineAreaDom.children[mine].innerHTML = '&#128163;';
+    };
+    for(let i = 0; i < mineAreaDom.children.length; i++) {
+      mineAreaDom.children[i].disabled = true;
+    };
+    stopGame();
+    loose ();  
+  }
+  else{
+    if (count(x, y) === 0){
+      let zeros = countAllZeros(x, y);
+          console.log('zeros =', zeros);
+      for(let i of zeros) {
+        countOpenCells.push(i);
+        disabledArrey.push(i);
+        let сx = Math.floor (i /10) +1;
+        let сy = i - Number((сx-1)*10) + 1;
+        event.target.innerHTML = '';
+        if(count(сx, сy) === 0) { 
+          mineAreaDom.children[i].innerHTML = '';
+          stateCells.push(mineAreaDom.children[i].innerHTML);
           mineAreaDom.children[i].disabled = true;
-        };
-        stopGame();
-        let time = Number(second.innerHTML)+Number(minute.innerHTML)
-        console.log(`${time}`)
-        image.src = 'img/loose.png';
-        info.innerHTML = `К сожалению, Вы проиграли. Время игры - ${time} сек.`;
-        popup.style.display ='block';
-        mineAreaDom.disabled = true;
-        document.body.style.overflow = "hidden"
-        smile.disabled = true;
-        
-      }
-      else{
-        if (count(x, y) === 0){
-          let zeros = countAllZeros(x, y);
-          
-        //   console.log('zeros =', zeros);
-          for(let i of zeros) {
-            countOpenCells.push(i);
-            let сx = Math.floor (i /10) +1;
-            let сy = i - Number((сx-1)*10) + 1;
-                event.target.innerHTML = '';
-                if(count(сx, сy) === 0) { 
-                  mineAreaDom.children[i].innerHTML = '';
-                  mineAreaDom.children[i].disabled = true;
-                  event.target.disabled = true; }
-                else{
-                  mineAreaDom.children[i].innerHTML = count(сx, сy);
-                  mineAreaDom.children[i].disabled = true;
-                  event.target.disabled = true;
-                }       
-          }
-        }
-        else {    
-          event.target.innerHTML =  count(x, y);
+          event.target.disabled = true; }
+        else{
+          mineAreaDom.children[i].innerHTML = count(сx, сy);
+          mineAreaDom.children[i].disabled = true;
           event.target.disabled = true;
-          countOpenCells.push(coorBtn);
-        }
+          stateCells.push(mineAreaDom.children[i].innerHTML);
+        }       
       }
+      
     }
-  
+    else {    
+      event.target.innerHTML =  count(x, y);
+      stateCells.push(mineAreaDom.children[coorBtn].innerHTML);
+      event.target.disabled = true;
+      countOpenCells.push(coorBtn);
+      disabledArrey.push(coorBtn);
+    }
+  }
   countOpenCells = Array.from(new Set(countOpenCells));
   if (countOpenCells.length === 90) {
     stopGame();
-    image.src = 'img/win.png'
-    alert('you win')
-  }
-
+    win();
+  } 
+  console.log(stateCells, 'stateCells.push(mineAreaDom.children[i].innerHTML);');
+  console.log(disabledArrey, 'disabledArrey');
+  localStorage.setItem('disabled',disabledArrey.toString());
+  localStorage.setItem('inner', stateCells.toString())
 });
 
+function win () {
+  smile.innerHTML = '&#128526;'
+  src = 'audio/win.mp3';
+  soundClick(src);
+  let timeW = Number(second.innerHTML)+Number(minute.innerHTML)*60;
+  info.innerHTML = `Вы выиграли. <br/>Ходов - ${countClick}. <br/>Время игры - ${timeW} сек.`;
+  popup.style.display ='block';
+  popup.style.zIndex ='0';
+  mineAreaDom.disabled = true;
+  document.body.style.overflow = "hidden"
+  smile.disabled = true;
+  localStorage.setItem('bombArrey',''); 
+}
+
+function loose () {
+  src = 'audio/lose.mp3';
+  soundClick(src);
+  let time = Number(second.innerHTML)+Number(minute.innerHTML)*60;
+  smile.innerHTML = '	&#128561;';
+    //image.src = 'img/loose.png';
+  info.innerHTML = `Вы проиграли. <br/>Ходов - ${countClick}. <br/>Время игры - ${time} сек.`;
+  popup.style.display ='block';
+  mineAreaDom.disabled = true;
+  document.body.style.overflow = "hidden"
+  smile.disabled = true; 
+  popup.style.zIndex ='0'; 
+  localStorage.setItem('bombArrey','');
+  }
 
 function checkBomb (x, y) {
   x = Number(x); 
@@ -245,7 +315,6 @@ function count(x, y) {
     }
   return count;
 }
-// ВЫНЕСТИ ЦИКЛЫ В ФУНКЦИЮ И ПОДАВАТЬ В IF ОТДЕЛЬНО
 function countZero(x, y) {
   x = Number(x)
   y = Number(y)
@@ -256,15 +325,17 @@ function countZero(x, y) {
     for(let i = 0; i <= 1; i++) {
       for(let j = 0; j <= 1; j++) { 
         coorBtn1 = mod(Number(y) - 1 + i, 10) + (Number(x) - 1 + j) * 10; 
-        countZero.push(coorBtn1);
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
+        countZero.push(coorBtn1);}
       }
     }
   }
   else if (( x === 10) && ( y=== 10)) {
         for(let i = -1; i <= 0; i++) {
           for(let j = -1; j <= 0; j++) {    
-            coorBtn1 = mod(Number(y) - 1 + i, 10) + (Number(x) - 1 + j) * 10; 
-            countZero.push(coorBtn1);           
+            coorBtn1 = mod(Number(y) - 1 + i, 10) + (Number(x) - 1 + j) * 10;
+            if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') { 
+            countZero.push(coorBtn1);}           
       }
     }
   }
@@ -272,9 +343,10 @@ function countZero(x, y) {
     for(let i = -1; i<= 0; i++) {
       for(let j = -1; j <= 1; j++) { 
         coorBtn1 = mod (Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
           countZero.push(coorBtn1);
-        }
+        }}
       }
     }
   }
@@ -282,9 +354,10 @@ function countZero(x, y) {
     for(let i = -1; i <= 1; i++) {
       for(let j = -1; j <= 0; j++) {    
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
           countZero.push(coorBtn1); 
-        } 
+        } }
       }
     }
   }
@@ -292,9 +365,10 @@ function countZero(x, y) {
     for(let i = -1; i <= 1; i++) {
       for(let j = 0; j <= 1; j++) {    
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
             countZero.push(coorBtn1);
-        } 
+        } }
       }
     }
   }
@@ -302,9 +376,10 @@ function countZero(x, y) {
     for(let i = -1; i <= 0; i++) {
       for(let j = 0; j <= 1; j++) {      
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
           countZero.push(coorBtn1);
-        } 
+        } }
       }
     }
   }    
@@ -312,9 +387,10 @@ function countZero(x, y) {
     for(let i = 0; i <= 1; i++) {
       for(let j = -1; j <= 0; j++) {  /// проверка на крайние значения      
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
             countZero.push(coorBtn1);
-        } 
+        } }
       }
     }
   }
@@ -322,9 +398,10 @@ function countZero(x, y) {
     for(let i = 0; i <= 1; i++) {
       for(let j = -1; j <= 1; j++) {  /// проверка на крайние значения      
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
           countZero.push(coorBtn1); 
-        } 
+        } }
       }
     }
   }
@@ -332,9 +409,10 @@ function countZero(x, y) {
     for(let i = -1; i <= 0; i++) {
       for(let j = -1; j <= 1; j++) {  /// проверка на крайние значения        
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
         if (coorBtn1 >= 0) {
           countZero.push(coorBtn1);
-        } 
+        } }
       }
     }
   }
@@ -342,7 +420,8 @@ function countZero(x, y) {
     for(let i = -1; i <= 1; i++) {
       for(let j = -1; j <= 1; j++) {  /// проверка на крайние значения      
         coorBtn1 = mod(Number(y) - 1 + i, 10) + Number((Number(x) - 1 + j)*10); 
-        countZero.push(coorBtn1); 
+        if (mineAreaDom.children[coorBtn1].innerHTML != '🚩') {
+        countZero.push(coorBtn1); }
       }
     }
   }
@@ -358,13 +437,11 @@ function mod(n, m) {
 function countAllZeros(x, y) {
   let visitedZeros = [];
   visitedZeros.push(calculateIndexFromCoordinates(x, y));
-  console.log(visitedZeros, typeof(visitedZeros))
   let zeros = countZero(x, y);
   let startSize = -1;
   let endSize = -1;
   do {
     startSize = zeros.length;
-    console.log(visitedZeros, zeros, startSize, endSize);
     for (let element of zeros) {
       let [xe, ye] = getCoordinates(element)
         if (!visitedZeros.includes(element) && count(xe, ye) === 0) {
@@ -378,15 +455,14 @@ function countAllZeros(x, y) {
           }
         }
     }
-        endSize = zeros.length;
+    endSize = zeros.length;
   } while (startSize != endSize);
-
   return zeros;
 }
 
 function calculateIndexFromCoordinates(x, y) {
   return mod(Number(y) - 1, 10) + Number((Number(x) - 1)*10);
-  }
+}
 
 function getCoordinates(index) {
   const x = Math.floor(index / 10) + 1; 
@@ -396,6 +472,87 @@ function getCoordinates(index) {
 
 
 function addFlag() {
-
+  let flagCount = flag.innerHTML;
+  return Number(flagCount)
 }
-  
+
+
+let countLongLeftMousClick =0;
+
+mineAreaDom.addEventListener('contextmenu', (event)=>{
+  event.preventDefault();
+  src = 'audio/tick.mp3';
+  soundClick(src);
+  if (!startGame) {
+    startGameTimer();
+  }
+  if (event.target.innerHTML === '🚩') {
+
+    flag.innerHTML = addFlag() + 1;
+    event.target.innerHTML='';
+    event.target.disabled =false;
+    console.log(event.target.innerHTML, 'right') 
+  }
+  else { 
+    if (flag.innerHTML === '0') 
+    {}
+    else if (event.target.disabled === true) 
+    {}
+
+    else {
+      flag.innerHTML = addFlag() - 1;
+      const btnClass = event.target;
+      const coordinate = btnClass.dataset.coordinates;
+      let [x, y] = coordinate.split(":");
+      const coorBtn = Number((y - 1) % 10) + Number((x - 1) * 10);
+      event.target.innerHTML = '&#128681;'; 
+      event.target.disabled = true;}
+  }
+})
+
+
+smile.addEventListener('click',(e)=> {
+    //document.location.reload();
+    src = 'audio/start.mp3'
+    soundClick(src); 
+    flag.innerHTML = 10;
+    countLongLeftMousClick =0;
+    countOpenCells = [];
+    bomb =[];
+    second.innerHTML = '00';
+    minute.innerHTML = '00';
+    countClick = 0;
+    timer = 0;
+    ms = 0;
+    sec = document.getElementById('second');
+    min = document.getElementById('minute');
+    startGame = false;  
+    bombIsReady = false;
+    mineAreaDom.disabled = false;
+    disabledArrey = [];
+    stateCells =[];
+
+    smile.innerHTML = '	&#129488;';
+    localStorage.setItem('bombArrey',bomb.toString());
+    localStorage.setItem('disabled',disabledArrey.toString());
+    localStorage.setItem('inner', stateCells.toString())
+    
+    for (let cell =0; cell < 100 ; cell++) {
+        //console.log (cell , 'cell ' )
+
+      if (mineAreaDom.children[cell].innerHTML || mineAreaDom.children[cell].disabled) {
+        mineAreaDom.children[cell].innerHTML = '';
+        mineAreaDom.children[cell].disabled = false;
+
+      }
+    }
+  });
+
+
+  //===========audio========
+  function soundClick(src) {
+    var audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = src; // Указываем путь к звуку "клика"
+    audio.autoplay = true; // Автоматически запускаем
+  }
+
